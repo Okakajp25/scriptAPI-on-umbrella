2583734c2dc9dac6f234b17d17e23e745d5409c4
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2023-08-02 16:30:34 +0900

    update

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import * as MC from "@minecraft/server";
 import { ActionFormData } from "@minecraft/server-ui";
+ 
+const world = MC.world
+
+world.beforeEvent.chatSend.subscribe(ev => {
+    const { sender, messege } = ev;
+    if( messege = '.help') {
+        sender.runCommandAsync("function help")
+    }
+})
